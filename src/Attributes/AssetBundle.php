0ea302a4be259fe507074cd48ed5c7c0decf3e18
Martin Procházka <juniwalk@outlook.cz>
2024-04-20 22:48:34 +0200

    Add new attribute for bundle importing

---
+++ src/Attributes/AssetBundle.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+/**
+ * @copyright (c) Martin Procházka
+ * @license   MIT License
+ */
+
+namespace JuniWalk\Tessa\Attributes;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_CLASS|Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
+class AssetBundle
+{
+	public function __construct(
+		public readonly string $bundleName,
+	) {
+	}
+}
