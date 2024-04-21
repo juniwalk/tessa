cd8c6ce57c8890d1c85467a02396b933c4116732
juniwalk <juniwalk@outlook.cz>
2024-04-21 23:11:38 +0200

    Remove custom  values

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -15,17 +15,6 @@ if (@!include __DIR__.'/../vendor/autoload.php') {
 	exit(1);
 }
 
-// Setup environment
-$_SERVER = [
-	'HTTPS' => 'On',
-	'HTTP_HOST' => 'nette.org:8080',
-	'QUERY_STRING' => 'view=default',
-	'REMOTE_ADDR' => '192.168.188.66',
-	'REQUEST_METHOD' => 'GET',
-	'REQUEST_URI' => '/index.php?view=default',
-	'SCRIPT_NAME' => '/index.php',
-];
-
 const DocumentRoot = __DIR__.'/.documentRoot';
 const TemporaryDir = DocumentRoot.'/temp';
 const AssetsStorage = DocumentRoot.'/assets';
